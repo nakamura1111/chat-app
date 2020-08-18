b08c7023617b7284989258dd27ee8eaff9aea121
nakamura1111 <5040.armuakan@gmail.com>
2020-08-18 18:10:38 +0900

    room_usersテーブルの文法エラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :name, presence: true
-  has_many: :room_users
-  has_many: :roooms, through: :room_users
+  has_many :room_users
+  has_many :roooms, through: :room_users
 end
