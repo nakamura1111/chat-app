ef941e091ec0b7f80ee89ff49ba289d1654e3cc1
nakamura1111 <5040.armuakan@gmail.com>
2020-08-19 12:02:04 +0900

    messageテーブルの作成（アソシエーション設定）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_081344) do
+ActiveRecord::Schema.define(version: 2020_08_19_023612) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.integer "room"
+    t.integer "user"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id"
